38af29d925ef78d652e60270358e525fba4c4f37
luying <2225745210@qq.com>
2016-06-14 17:24:36 +0800

    完成

--- app/src/main/java/liangkang/com/shouhutianshi/base/Angle_Of_Children.java
+++ app/src/main/java/liangkang/com/shouhutianshi/base/Angle_Of_Children.java
@@ -10,28 +10,32 @@ import android.widget.Toast;
  */
 public class Angle_Of_Children extends Application {
     private static Context context;
-    private static String SHOUHUTIANSHI="The Angle Of Children";//文件名
-    private static String ISFIRST="IsFirst";
+    private static String SHOUHUTIANSHI = "The Angle Of Children";//文件名
+    private static String ISFIRST = "IsFirst";
+
     @Override
     public void onCreate() {
         super.onCreate();
-        context=this;
+        context = this;
     }
-    public static Context getContext(){
+
+    public static Context getContext() {
         return context;
     }
-    public static boolean isFirst(){
-        SharedPreferences sharedPreferences=context.getSharedPreferences(SHOUHUTIANSHI,MODE_PRIVATE);
-        boolean isFirst=sharedPreferences.getBoolean(ISFIRST,true);
-        if (isFirst){
-            SharedPreferences.Editor editor=sharedPreferences.edit();
-            editor.putBoolean(ISFIRST,false).apply();
+
+    public static boolean isFirst() {
+        SharedPreferences sharedPreferences = context.getSharedPreferences(SHOUHUTIANSHI, MODE_PRIVATE);
+        boolean isFirst = sharedPreferences.getBoolean(ISFIRST, true);
+        if (isFirst) {
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+            editor.putBoolean(ISFIRST, false).apply();
 
         }
         return isFirst;
     }
-    public static void makeToast(String message){
-        Toast.makeText(context,message,Toast.LENGTH_SHORT).show();
+
+    public static void makeToast(String message) {
+        Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
 
 
     }
