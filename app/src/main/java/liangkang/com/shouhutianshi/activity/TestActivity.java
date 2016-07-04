ac39a57dbcb721574876998739dfeeb149b6b695
luying <2225745210@qq.com>
2016-07-04 17:27:20 +0800

    2.1

---
+++ app/src/main/java/liangkang/com/shouhutianshi/activity/TestActivity.java
@@ -0,0 +1,9 @@
+package liangkang.com.shouhutianshi.activity;
+
+import android.luying.com.datareport.BaseLuyingActivity;
+
+/**
+ * Created by luying on 16/7/4.
+ */
+public class TestActivity extends BaseLuyingActivity {
+}
---
+++ datareport/src/main/java/android/luying/com/datareport/BaseLuyingActivity.java
@@ -0,0 +1,10 @@
+package android.luying.com.datareport;
+
+import android.app.Activity;
+
+/**
+ * Created by luying on 16/7/4.
+ */
+public class BaseLuyingActivity extends Activity{
+
+}
